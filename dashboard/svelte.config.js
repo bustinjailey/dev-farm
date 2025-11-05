659270d8c6bb2c8127340834b2d381dc7fdfd976
Justin Bailey <bustinjailey@gmail.com>
2025-11-04 18:27:52 -0800

    fix: Clean up Svelte config, use default Svelte 5 runes mode

--- dashboard/svelte.config.js
+++
@@ -1,8 +0,0 @@
-import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
-
-export default {
-  preprocess: vitePreprocess(),
-  compilerOptions: {
-    runes: false, // Disable Svelte 5 runes mode to use legacy reactivity
-  },
-};
